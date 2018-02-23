0e038e91178e78ff73de3121852f7b8ce5b40d6d
Tim Kiely <thetimkiely@gmail.com>
2018-02-22 21:36:04 -0500

    AWS testing

--- rembrandtml/model_implementations/test_model.py
+++ rembrandtml/model_implementations/test_model.py
@@ -1,8 +1,12 @@
+import os, sys
+root_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
+print(root_dir)
+sys.path.append(root_dir)
+print(f'path: {sys.path}')
 from rembrandtml.configuration import DataConfig
 
-
 def main():
-    data_config = DataConfig()
+    data_config = DataConfig('f', 'd')
     print('This is main')
 
 if __name__ == '__main__':

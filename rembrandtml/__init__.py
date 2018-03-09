f8a85aae1bb03a37ab1e493d6d6d3eb3b125d80c
Tim Kiely <thetimkiely@gmail.com>
2018-03-09 10:44:41 -0500

    AWS troubles

--- rembrandtml/__init__.py
+++ rembrandtml/__init__.py
@@ -1,5 +1,6 @@
 import sys
 import os
+print('Running __init__.py')
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(os.path.dirname(__file__), "."))
